6be888a8417736096b7f0043e91c2dcf5f337da2
maouzal-pandu <maozalpandu123@gmail.com>
2026-08-10 18:25:52 +0700

    feat: Add kehamilan table model

---
+++ models/kehamilan.py
@@ -0,0 +1,40 @@
+from sqlalchemy import (
+    DECIMAL,
+    Column,
+    Date,
+    DateTime,
+    Enum,
+    Float,
+    ForeignKey,
+    Integer,
+    func,
+)
+from sqlalchemy.orm import relationship
+
+from config.database import Base
+
+
+class Kehamilan(Base):
+    __tablename__ = "kehamilan"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False, index=True)
+    hpht = Column(Date, nullable=False)
+    bb_awal = Column(DECIMAL(5, 2), nullable=False)
+    gravida = Column(Integer, nullable=False)
+    paritas = Column(Integer, nullable=False)
+    abortus = Column(Integer, nullable=False)
+    status_kehamilan = Column(
+        Enum("berlangsung", "selesai", name="kehamilan_enum"),
+        default="berlangsung",
+        nullable=False,
+    )
+    status_risiko = Column(
+        Enum("rendah", "sedang", "tinggi", name="risiko_enum"),
+        default="rendah",
+        nullable=False,
+    )
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+
+    user = relationship("User", backref="kehamilan_list")
