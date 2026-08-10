d835c41fe58f969262f429842e7a6bfe3cb776fe
maouzal-pandu <maozalpandu123@gmail.com>
2026-08-10 20:51:26 +0700

    feat: Import models

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
 from models.user import User
 from models.kehamilan import Kehamilan
 from models.pemeriksaan import Pemeriksaan
+from models.wilayah import Desa
+from models.verify_otp import VerifyOtp
