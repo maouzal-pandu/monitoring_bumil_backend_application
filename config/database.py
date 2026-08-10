e8fca914b8aeb9397e2f17aa0ea45db9ede4d80e
maouzal-pandu <maozalpandu123@gmail.com>
2026-08-10 20:54:05 +0700

    refactor: Move credentials

--- config/database.py
+++ config/database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/monitoring_bumil_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL belum di set")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
