ee2d2a5b0971c54c08271cb0e4f398f9004a582a
maouzal-pandu <maozalpandu123@gmail.com>
2026-08-10 15:31:04 +0700

    chore: Setup db connection

--- config/database.py
+++ config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://user:root@localhost/monitoring_bumil_db"
+DATABASE_URL = "mysql+pymysql://root:@localhost/monitoring_bumil_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
