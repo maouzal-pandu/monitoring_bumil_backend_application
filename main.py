513c442cf1702b8f9dea3f6ce9487889923350a4
maouzal-pandu <maozalpandu123@gmail.com>
2026-08-11 18:46:57 +0700

    feat: Add auth router

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from config.database import Base, engine
 import models
-
-# from routers import
+from routers import auth_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-# app.include_router(user.router)
+app.include_router(auth_router)
